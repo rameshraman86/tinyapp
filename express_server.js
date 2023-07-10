32749e9f481d30dcf70f3233fbca669f1d7af5a1
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-10 14:37:30 -0400

    added initial ejs

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
+app.set('view engine', 'ejs');
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -19,6 +22,17 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//send the urldatabase to url_index file and then render it in the browser in /urls endpoint.
+app.get('/urls', (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get('/urls/:id', (req, res) => {
+  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app Listening to port: ${PORT}`);
-})
+});
