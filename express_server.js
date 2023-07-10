424f39e667003ae1d64a0c93a8652e08b376dc0a
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-10 16:48:06 -0400

    added POST method

--- express_server.js
+++ express_server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
 const PORT = 8080;
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
+
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let randomString = '';
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters.charAt(randomIndex);
+  }
+  return randomString;
+}
 
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "4facas": "http://www.reddit.com"
 };
 
+//GET METHODS
 app.get('/', (req, res) => {
   res.send('Hello');
 });
@@ -28,11 +42,26 @@ app.get('/urls', (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+  res.render("urls_new");
+});
 
 app.get('/urls/:id', (req, res) => {
   const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
   res.render("urls_show", templateVars);
 });
+
+
+//POSTS
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app Listening to port: ${PORT}`);
