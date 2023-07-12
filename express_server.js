17a1a35cbd5f03b3fecf3070218e07c50da759c8
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-12 05:56:35 -0400

    modified comments

--- express_server.js
+++ express_server.js
@@ -25,61 +25,65 @@ const urlDatabase = {
   "4facas": "http://www.reddit.com"
 };
 
-//GET METHODS
+//homepage. Redirects to the /urls to show all urls of logged in user.
 app.get('/', (req, res) => {
   res.redirect('/urls');
 });
 
 //send the urldatabase to url_index.ejs file and then render it in the browser in /urls endpoint.
 app.get('/urls', (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     username: req.cookies["username"],
     urls: urlDatabase
-   };
+  };
   res.render("urls_index", templateVars);
 });
 
+//page to add new URL
 app.get('/urls/new', (req, res) => {
   const templateVars = {
     username: req.cookies["username"]
-  }
+  };
   res.render("urls_new", templateVars);
 });
 
+//page that opens the details page of a tinyURL.
 app.get('/urls/:id', (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     username: req.cookies["username"],
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id] 
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id]
   };
   res.render("urls_show", templateVars);
 });
 
-//open the url when passed the ID
+//Will get the actual url when passed the tinyURL ID.
 app.get("/u/:id", (req, res) => {
   if (!urlDatabase[req.params.id]) {
     res.status(404).send(`${req.params.id} is not created yet. `);
     return;
   }
   const longURL = urlDatabase[req.params.id];
+  console.log(typeof(longURL));
   res.redirect(longURL);
 });
 
-//add a new tiny url
+
+//Add a new tinyURL
 app.post("/urls", (req, res) => {
   const URLCode = generateRandomString();
   urlDatabase[URLCode] = req.body.longURL;
   res.redirect(`/urls/${URLCode}`);
 });
 
-//DELETE A TINYURL
+//Delete a TinyURL
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id];
   res.redirect("/urls");
 });
 
 
-//UPDATE a long url name
+//Update a tinyURL
 app.post("/urls/:id/update", (req, res) => {
   urlDatabase[req.params.id] = req.body.longURLUpdated;
   res.redirect("/urls");
@@ -92,23 +96,13 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+//Logout and clear cookies
 app.post("/logout", (req, res) => {
   res.clearCookie("username");
   res.redirect("/urls");
 });
 
 
-
 app.listen(PORT, () => {
   console.log(`Example app Listening to port: ${PORT}`);
 });
-
-
-
-// app.get('/urls.json', (req, res) => {
-//   res.json(urlDatabase);
-// });
-
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
