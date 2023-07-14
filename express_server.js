fca048dfa774484ed6fda7e93be57c26e76087f8
Ramesh Raman <2029381+rameshraman86@users.noreply.github.com>
2023-07-13 21:19:55 -0400

    changed encryption of cookies to cookie-session

--- express_server.js
+++ express_server.js
@@ -1,18 +1,24 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 const salt = bcrypt.genSaltSync(10);
+const cookieSession = require('cookie-session');
 
-const { findUserByEmail, generateRandomString, urlsForUser } = require("./helperFunctions");
+const { findUserByEmail, generateRandomString, urlsForUser } = require('./helperFunctions');
+
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(cookieParser());
+
+app.use(cookieSession({
+  name: 'session',
+  secret: 'xyZAbC',
+  maxAge: 24 * 60 * 60 * 1000,
+})
+);
 
 app.set('view engine', 'ejs');
 
 const PORT = 8080;
-
 
 const urlDatabase = {
   "b2xVn2": {
@@ -57,17 +63,17 @@ const users = {
 };
 
 
-
 /*
 REGISTRATION
 */
 app.get('/register', (req, res) => {
-  const userID = req.cookies["user_id"];
+  console.log(users);
+  const userID = req.session.user_id;
 
   if (userID) {
     return res.redirect('/urls');
   }
-  res.render("register", { currentPage: 'register' });
+  res.render('register', { currentPage: 'register' });
 });
 
 app.post('/register', (req, res) => {
@@ -78,32 +84,30 @@ app.post('/register', (req, res) => {
     return res.status(400).send('Email or Password cannot be empty');
   }
 
-  if (findUserByEmail(email, users) === null) { //user does not exist already, create user profile
+  if (findUserByEmail(email, users) === null) {
     const generatedRandomUserID = generateRandomString(6);
     users[generatedRandomUserID] = {
       id: generatedRandomUserID,
       email: email.toLowerCase(),
-      password: password
+      password: password,
     };
-    res.cookie("user_id", generatedRandomUserID);
-    return res.redirect("/urls");
+    req.session.user_id = generatedRandomUserID;
+    return res.redirect('/urls');
   }
 
   return res.status(400).send('User already exists.');
 });
 
-
-
 /*
 USER LOGIN
 */
 app.get('/login', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
 
   if (userID) {
-    return res.redirect("/urls");
+    return res.redirect('/urls');
   }
-  return res.render("login", { currentPage: 'login' });
+  return res.render('login', { currentPage: 'login' });
 });
 
 app.post('/login', (req, res) => {
@@ -114,78 +118,75 @@ app.post('/login', (req, res) => {
     return res.status(400).send('Email or Password cannot be empty');
   }
 
-  //use the email to find user record from users database
   const userRecordInDatabase = findUserByEmail(email.toLowerCase(), users);
 
-  //user name and password match. All good.
-  if (userRecordInDatabase !== null && bcrypt.compareSync(userEnteredPassword, userRecordInDatabase.password)) {
-    res.cookie("user_id", userRecordInDatabase.id);
-    return res.redirect("/urls");
+  if (
+    userRecordInDatabase !== null &&
+    bcrypt.compareSync(userEnteredPassword, userRecordInDatabase.password)
+  ) {
+    req.session.user_id = userRecordInDatabase.id;
+    return res.redirect('/urls');
   }
 
-  //username exists but password does not match
-  if (userRecordInDatabase !== null && !bcrypt.compareSync(userEnteredPassword, userRecordInDatabase.password)) {
-    return res.status(403).send("Login Failed. Incorrect Username or Password.");
-  }
-
-  //user does not exist
-  if (userRecordInDatabase === null) {
-    return res.status(403).send('Login Failed. Incorrect Username or Password.');
-  }
+  return res.status(403).send('Login Failed. Incorrect Username or Password.');
 });
-
 
 /*
 HOMEPAGE - SEE ALL URLS
 */
 app.get('/', (req, res) => {
-  res.redirect('/urls');
+  const userID = req.session.user_id;
+  if(userID){
+    res.redirect('/urls');
+  }
+  res.redirect('/login');
 });
 
 app.get('/urls', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
 
   if (userID === undefined) {
-    return res.send('<html><h1>Lost your way?</h1><h3>You must be signed in to view your URLs. Please register if you have not already and sign in to continue.</h3><a href ="/login">Back to login page</a></body></html>');
+    return res.send(
+      '<html><h1>Lost your way?</h1><h3>You must be signed in to view your URLs. Please register if you have not already and sign in to continue.</h3><a href ="/login">Back to login page</a></body></html>'
+    );
   }
 
   const templateVars = {
     userID: userID,
     user: users,
     urls: urlsForUser(userID, urlDatabase),
-    currentPage: 'URLIndex'
+    currentPage: 'URLIndex',
   };
 
-  res.render("urls_index", templateVars);
+  res.render('urls_index', templateVars);
 });
-
-
 
 /*
 ADD NEW URL
 */
 app.get('/urls/new', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
 
   if (userID === undefined) {
-    return res.redirect("/login");
+    return res.redirect('/login');
   }
 
   const templateVars = {
     userID: userID,
     user: users,
-    currentPage: 'AddNewURL'
+    currentPage: 'AddNewURL',
   };
-  res.render("urls_new", templateVars);
+  res.render('urls_new', templateVars);
 });
 
 app.post('/urls', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
 
   if (userID === undefined) {
-    return res.send('<html><body><h1>Lost your way?</h1><h3>You must be signed in to create tiny URL. Please register if you have not already and sign in to continue.</h3></body></html>');
-  }
-  else {
+    return res.send(
+      '<html><body><h1>Lost your way?</h1><h3>You must be signed in to create tiny URL. Please register if you have not already and sign in to continue.</h3></body></html>'
+    );
+  } else {
     const tinyURLID = generateRandomString(6);
     const incominglongURL = req.body.longURL;
     urlDatabase[tinyURLID] = { longURL: incominglongURL, userID };
@@ -194,117 +195,108 @@ app.post('/urls', (req, res) => {
   }
 });
 
-
-
 /*
 VIEW DETAILS AND EDIT URL
 */
-//page that opens the details page of a tinyURL.
 app.get('/urls/:id', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const urlID = req.params.id;
 
   if (!userID) {
-    return res.send('<html><h1>Lost your way?</h1><h3>You must be signed in to view URL details. <a href ="/login">Back to login page</a></body></html>');
+    return res.send(
+      '<html><h1>Lost your way?</h1><h3>You must be signed in to view URL details. <a href ="/login">Back to login page</a></body></html>'
+    );
   }
 
-  //TinyURL does not exist. 
   if (!urlDatabase[urlID]) {
-    return res.status(400).send(`<html><h3>The TinyURL you entered does not exist.</h3><a href ="/login">Back to login page</a></body></html>'`);
+    return res.status(400).send(
+      `<html><h3>The TinyURL you entered does not exist.</h3><a href ="/login">Back to login page</a></body></html>'`
+    );
   }
 
-  //Check if the tinyURL belongs to the logged in user. if not, error
   if (urlDatabase[urlID].userID !== userID) {
-    return res.send('<html><h1>Invalid Request</h1><h3>You do not have access to the TinyURL details.</h3></body></html>');
+    return res.send(
+      '<html><h1>Invalid Request</h1><h3>You do not have access to the TinyURL details.</h3></body></html>'
+    );
   }
-  //tinyURL belongs to logged in user. So, go ahead and render the page with these variables.
+
   const templateVars = {
     userID: userID,
     user: users,
     id: urlID,
     longURL: urlDatabase[urlID].longURL,
-    currentPage: 'URLDetails'
+    currentPage: 'URLDetails',
   };
-  res.render("urls_show", templateVars);
+  res.render('urls_show', templateVars);
 });
 
 app.post('/urls/:id', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const urlID = req.params.id;
 
-  //nobody is logged in. Error out.
   if (!userID) {
     return res.send('Error. You must be signed in to view or edit URL details.');
   }
-  //TinyURL does not exist in the database
+
   if (!urlDatabase[urlID]) {
     return res.status(400).send(`The requested TinyURL does not exist.`);
   }
-  //logged in user doesn't have access to the tinyURL that they are trying to open
+
   if (urlDatabase[urlID].userID !== userID) {
     return res.send('Invalid Request. You do not have access to the TinyURL details.');
   }
-  //tinyURL exists, user is logged in and they own this URL. update the URL when update is pressed.
+
   urlDatabase[urlID].longURL = req.body.longURLUpdated;
-  res.redirect("/urls");
-
+  res.redirect('/urls');
 });
-
 
 /*
 OPEN THE full URL using tinyURL
 */
 app.get('/u/:id', (req, res) => {
   const templateVars = {
-    currentPage: 'URLPage'
+    currentPage: 'URLPage',
   };
   if (!urlDatabase[req.params.id]) {
-    res.status(404).send(`<html><body><h1>ID does not exist.</h1><h3>The ID you entered <i>\"${req.params.id}\" </i>does not exist.</h3></body></html>`);
+    res.status(404).send(
+      `<html><body><h1>ID does not exist.</h1><h3>The ID you entered <i>"${req.params.id}" </i>does not exist.</h3></body></html>`
+    );
     return;
   }
   const longURL = urlDatabase[req.params.id].longURL;
   res.redirect(longURL);
 });
 
-
-
 /*
 DELETE A TINY URL
 */
 app.post('/urls/:id/delete', (req, res) => {
-  const userID = req.cookies["user_id"];
+  const userID = req.session.user_id;
   const urlObj = urlDatabase[req.params.id];
 
-  //No user is logged in
   if (!userID) {
     return res.status(400).send('You must be signed in to delete URL.');
   }
 
-  //tinyURL ID doesn't exist in database
   if (!urlObj) {
     return res.status(400).send('The requested ID does not exist.');
   }
 
-  //User is trying to delete tinyURL that doesn't belong to them
   if (urlObj.userID !== userID) {
     return res.send('Invalid Request. You do not have access to this TinyURL.');
   }
 
-
   delete urlDatabase[req.params.id];
   return res.redirect('/urls');
-
 });
-
 
 /*
 LOGOUT AND CLEAR COOKIES
 */
 app.post('/logout', (req, res) => {
-  res.clearCookie("user_id");
-  res.redirect("/login");
+  req.session = null;
+  res.redirect('/login');
 });
-
 
 app.listen(PORT, () => {
   console.log(`Example app Listening to port: ${PORT}`);
